a1d33f7c979e5380086171af52c819a9e2841cff
Michele Bianchi <michele.bianchi@embark-studios.com>
2022-07-29 13:12:50 +0200

    Added clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use std::path::PathBuf;
 mod utils;
 
@@ -8,10 +8,24 @@ mod utils;
 struct Cli {
     #[clap(subcommand)]
     mode: Mode,
+}
 
+#[derive(Subcommand, Debug)]
+enum Mode {
+    /// Converts an assembly file to hex
+    Assemble(ModeConfig),
+    /// Disassembles a binary file to human-readable assembly file
+    Disassemble(ModeConfig),
+}
+
+#[derive(Debug, Args)]
+#[clap(args_conflicts_with_subcommands = true)]
+struct ModeConfig {
+    /// Path to the file where to write the result, if missing it will be output to stdin
     #[clap(short, long, value_parser, value_name = "OUTPUT")]
     output: Option<PathBuf>,
 
+    /// Base instruction pointer for (dis)assembly.
     #[clap(
         short,
         long,
@@ -21,20 +35,12 @@ struct Cli {
     )]
     base_pointer: u16,
 
+    /// Do not print logs or output
     #[clap(short, long, action)]
-    silent: bool,
-}
+    quiet: bool,
 
-#[derive(Subcommand, Debug)]
-enum Mode {
-    Assemble {
-        #[clap(parse(try_from_str=check_and_canonicalize), value_name = "SOURCE")]
-        file_path: Option<PathBuf>,
-    },
-    Disassemble {
-        #[clap(parse(try_from_str=check_and_canonicalize), value_name = "SOURCE")]
-        file_path: Option<PathBuf>,
-    },
+    #[clap(parse(try_from_str=check_and_canonicalize), value_name = "SOURCE")]
+    file_path: Option<PathBuf>,
 }
 
 fn check_and_canonicalize(s: &str) -> std::io::Result<PathBuf> {
